090e5f7d0cda25be06138c37c3fbbc225047b59e
Mehwish Aziz <mehwisha@msn.com>
2018-04-09 16:31:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["  ","  ","  ","  "," X ","  ","  ","  ","  "]
+
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
@@ -9,4 +9,5 @@ def display_board(board)
   puts "   |   |   "
 
 end
+board = [" O ","  ","  ","  "," X ","  ","  ","  ","  "]
 display_board(board)
