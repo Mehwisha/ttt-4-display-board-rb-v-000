a8c3ce7521d11d5b8fb9134471377b6963fee8fc
Mehwish Aziz <mehwisha@msn.com>
2018-04-09 16:20:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,9 @@
 board = ["  ","  ","  ","  "," X ","  ","  ","  ","  "]
 def display_board(board)
   puts "  |  |  "
-  puts "----------"
-  puts "  |   |   "
-  puts "----------"
-  puts "  |   |   "
-
+  puts "-----------"
+  puts "  |  |  "
+  puts "-----------"
+  puts "  |  |  "
 
 end
